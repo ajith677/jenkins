01d135dc9a6a5c63a1c8e78310391294371303d4
ajith677 <ajith.pathmanathan@incubatelabs.com>
2025-06-26 16:09:23 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,9 @@
+package com.example.demo.entity;
+
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+}
